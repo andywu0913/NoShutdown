2b1cbd26f6895ab3f19a9a13f752057108f04c09
Andy Wu <AndyWu@MacBookPro.local>
2019-04-20 01:10:57 -0500

    Migrate project to Github.

---
+++ NoShutDown/NoShutDown.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace NoShutdown
+{
+    public partial class NoShutDown : Form
+    {
+        [DllImport("user32.dll")]
+        public extern static bool ShutdownBlockReasonCreate(IntPtr hWnd, [MarshalAs(UnmanagedType.LPWStr)] string pwszReason);
+
+        public NoShutDown()
+        {
+            InitializeComponent();
+            //ShutdownBlockReasonCreate(Handle, "Don't let you shut down :P");
+        }
+
+        protected override void WndProc(ref Message aMessage)
+        {
+            const int WM_QUERYENDSESSION = 0x0011;
+            const int WM_ENDSESSION = 0x0016;
+
+            if (aMessage.Msg == WM_QUERYENDSESSION || aMessage.Msg == WM_ENDSESSION)
+                return;
+
+            base.WndProc(ref aMessage);
+        }
+
+        protected override CreateParams CreateParams //Hide from Task Manager
+        {
+            get
+            {
+                var cp = base.CreateParams;
+                cp.ExStyle |= 0x80;
+                return cp;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# NoShutDown
+# No ShutDown
+
+This tool does not have any GUI. It runs in the background which named as `explorer.exe` to bewilder users finding it.
+
+Whenever a user is trying to shutdown the system, a message dialog will pop up and thus prevent the system from shutting down.
+
+## Environment
+
+- Windows7
+- Microsoft Visual Studio 2017
+- C# .NET Framework 4.5.2
+
+## Screenshot
+
+<img src="https://github.com/andywu0913/NoShutDown/blob/master/demo.png" width="400px">
